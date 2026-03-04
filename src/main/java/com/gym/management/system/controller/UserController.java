cf5ee8d4221ebb61d2c5caa47d8f295149e828b2
sandeep <tiwarisandeep1909@gmail.com>
2026-03-04 21:36:32 +0530

    tied up a few things in user module

---
+++ src/main/java/com/gym/management/system/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.gym.management.system.controller;
+
+import com.gym.management.system.entity.User;
+import com.gym.management.system.service.interfaces.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        return userService.addUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@RequestBody User user, @PathVariable Long id) {
+        return userService.updateUser(user, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public User deleteUser(@PathVariable Long id) {
+        return userService.deleteUser(id);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+}
--- src/main/java/com/gym/management/system/entity/User.java
+++ src/main/java/com/gym/management/system/entity/User.java
@@ -9,7 +9,7 @@ import lombok.Data;
 public class User {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(unique = true)
