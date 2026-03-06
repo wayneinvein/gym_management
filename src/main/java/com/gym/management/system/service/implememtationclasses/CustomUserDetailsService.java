c6e7d106c8a2afc78b011ee8585a1ed925e5fd9d
sandeep <tiwarisandeep1909@gmail.com>
2026-03-06 15:41:25 +0530

    implemented CustomUserDetailsService

---
+++ src/main/java/com/gym/management/system/service/implememtationclasses/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.gym.management.system.service.implememtationclasses;
+
+import com.gym.management.system.entity.User;
+import com.gym.management.system.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getUserRole().name())
+                .build();
+    }
+}
