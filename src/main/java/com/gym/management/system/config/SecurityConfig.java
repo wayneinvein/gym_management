5b028763305c1c5305a8a9146f01b17c02446770
sandeep <tiwarisandeep1909@gmail.com>
2026-03-06 16:16:04 +0530

    updated SecurityConfig class

--- src/main/java/com/gym/management/system/config/SecurityConfig.java
+++ src/main/java/com/gym/management/system/config/SecurityConfig.java
@@ -1,16 +1,47 @@
 package com.gym.management.system.config;
 
+import com.gym.management.system.service.implememtationclasses.CustomUserDetailsService;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
+@AllArgsConstructor
 public class SecurityConfig {
+
+    private final CustomUserDetailsService customUserDetailsService;
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+
+                .authorizeHttpRequests(auth -> auth
+
+                        .requestMatchers("/users").permitAll()
+
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+
+                        .requestMatchers("/trainer/**").hasRole("TRAINER")
+
+                        .anyRequest().authenticated()
+                )
+
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
 }
