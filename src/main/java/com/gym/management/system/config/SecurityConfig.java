369d46375867b7021565745f848c273ffb048935
sandeep <tiwarisandeep1909@gmail.com>
2026-03-17 15:51:59 +0530

    added JWT dependency to pom.xml

--- src/main/java/com/gym/management/system/config/SecurityConfig.java
+++ src/main/java/com/gym/management/system/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import com.gym.management.system.service.implememtationclasses.CustomUserDetails
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
