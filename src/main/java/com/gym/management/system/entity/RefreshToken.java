471471f3a8256305547e7c76610f3227d2259d04
sandeep <tiwarisandeep1909@gmail.com>
2026-04-08 11:28:47 +0530

    added @Data in RefreshToken entity for getters and setters

--- src/main/java/com/gym/management/system/entity/RefreshToken.java
+++ src/main/java/com/gym/management/system/entity/RefreshToken.java
@@ -3,11 +3,13 @@ package com.gym.management.system.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 
 import java.time.Instant;
 
 @Entity
+@Data
 public class RefreshToken {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
