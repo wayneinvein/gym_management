7263f929f9685ad5feea8178cf203210b557439a
sandeep <tiwarisandeep1909@gmail.com>
2026-04-08 11:21:00 +0530

    updated service interface for refresh token

--- src/main/java/com/gym/management/system/service/interfaces/RefreshTokenService.java
+++ src/main/java/com/gym/management/system/service/interfaces/RefreshTokenService.java
@@ -2,8 +2,15 @@ package com.gym.management.system.service.interfaces;
 
 import com.gym.management.system.entity.RefreshToken;
 
+import java.util.Optional;
+
 public interface RefreshTokenService {
 
-    public RefreshToken createRefreshToken(String username);
-    public RefreshToken verifyExpiration(RefreshToken token);
+    RefreshToken createRefreshToken(String username);
+
+    RefreshToken verifyExpiration(RefreshToken token);
+
+    Optional<RefreshToken> findByToken(String token);
+
+    void deleteByUsername(String username);
 }
