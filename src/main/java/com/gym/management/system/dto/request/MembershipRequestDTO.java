a84ad48521cadac32c7bc37905ce4d0c54e838df
sandeep <tiwarisandeep1909@gmail.com>
2026-04-22 15:44:21 +0530

    changed MembershipRequestDTO

--- src/main/java/com/gym/management/system/dto/request/MembershipRequestDTO.java
+++ src/main/java/com/gym/management/system/dto/request/MembershipRequestDTO.java
@@ -1,25 +1,23 @@
 package com.gym.management.system.dto.request;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
 
 import java.time.LocalDate;
 
 /**
- * DTO for creating a membership record.
- * Captures the start date of the membership.
+ * DTO for creating a new membership subscription.
+ *
+ * Only startDate is required from the client.
+ * endDate is calculated automatically from plan duration.
+ * amountPaid is taken from the plan price at time of subscription.
+ * status is always set to ACTIVE on creation.
  */
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@Data
 public class MembershipRequestDTO {
 
-    // Start date of the membership (must not be null)
+    // Start date of the membership
+    // End date will be auto-calculated as startDate + plan.durationDays
     @NotNull(message = "Start date is required")
     private LocalDate startDate;
-
 }
