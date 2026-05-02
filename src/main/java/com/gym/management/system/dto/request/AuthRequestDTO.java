a97792e6207c217ec55d0660c93c041bd8619c4d
sandeep <tiwarisandeep1909@gmail.com>
2026-05-02 11:44:06 +0530

    added validations and pattern matching in AuthRequestDTO

--- src/main/java/com/gym/management/system/dto/request/AuthRequestDTO.java
+++ src/main/java/com/gym/management/system/dto/request/AuthRequestDTO.java
@@ -11,12 +11,10 @@ import lombok.Data;
 @Data
 public class AuthRequestDTO {
 
-    // Username of the user attempting to log in
     @NotBlank(message = "Username is required")
     @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
     private String username;
 
-    // Plain text password provided by the user (will be authenticated securely)
     @NotBlank(message = "Password is required")
     @Size(min = 6, max = 100, message = "Password must be between 6 and 100 characters")
     private String password;
