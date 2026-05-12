8ac90ac7324f9f652567f2fd0c8f618fe3d5636d
sandeep <tiwarisandeep1909@gmail.com>
2026-05-12 17:00:51 +0530

    solved merge conflict

--- src/main/java/com/gym/management/system/config/SwaggerConfig.java
+++ src/main/java/com/gym/management/system/config/SwaggerConfig.java
@@ -1,14 +1,15 @@
 package com.gym.management.system.config;
 
 import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import java.util.List;
 
 /**
@@ -26,7 +27,6 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                // API metadata displayed in Swagger UI
                 .info(new Info()
                         .title("Gym Management System APIs")
                         .version("1.0")
@@ -36,8 +36,6 @@ public class SwaggerConfig {
                                 .email("tiwarisandeep1909@gmail.com")
                                 .url("https://github.com/wayneinvein")))
 
-                // Server URLs — Swagger will use these to make API calls
-                // Production server must be https
                 .servers(List.of(
                         new Server()
                                 .url("https://sandeeptiwari.up.railway.app")
@@ -47,11 +45,8 @@ public class SwaggerConfig {
                                 .description("Local Development Server")
                 ))
 
-                // Enables global security requirement so all endpoints
-                // can use the configured JWT authentication
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
 
-                // Defines the JWT bearer authentication scheme
                 .components(new Components()
                         .addSecuritySchemes("bearerAuth",
                                 new SecurityScheme()
